5bf928a8b6915783eec766c8f78bf309b2ce13a3
Mariano <mariano.dominguez@adaptio.com>
2018-10-02 17:00:21 -0300

    [develop] add telegram bot

---
+++ dbSigesmen.py
@@ -0,0 +1,76 @@
+import MySQLdb
+import json
+
+DATABASE_FILE = "dbConf.json"
+
+
+INSERT_MESSAGE = "INSERT INTO mea_mensajes_alarma(mea_codigo_cliente, mea_grupo, mea_fecha, mea_hora, mea_contenido, mea_codigo_accion, mea_estado, mea_verificado) VALUES ({0}, 1, CURRENT_DATE(), CURRENT_TIME(), '{1}', 0, 1, 0)"
+GET_CLIENT = "SELECT EXISTS(SELECT * FROM cli_clientes WHERE cli_codigo =  {0} )"
+GET_CLAIM_ID = "SELECT men_id from men_mensajes WHERE men_origen_id = {0}"
+
+class Database(object):
+    __instance   = None
+    __host       = None
+    __user       = None
+    __password   = None
+    __database   = None
+    __session    = None
+    __connection = None
+
+
+    def __init__(self, user, password, host, port, database):
+        self.__user     = user
+        self.__password = password
+        self.__host     = host
+        self.__port     = port
+        self.__database = database
+    ## End def __init__
+
+    def __open(self):
+        try:
+            cnx = MySQLdb.connect(host = self.__host, user = self.__user, passwd = self.__password, db = self.__database, port = self.__port)
+            self.__connection = cnx
+            self.__session    = cnx.cursor()
+        except MySQLdb.Error as e:
+            print "Error %d: %s" % (e.args[0],e.args[1])
+    ## End def __open
+
+    def __close(self):
+        self.__session.close()
+        self.__connection.close()
+    ## End def __close
+    
+    def __selectOneRow(self, query):
+        self.__open()
+        self.__session.execute(query)
+        return self.__session.fetchone()[0]
+
+    def isCodeExists(self, code):
+        return self.__selectOneRow(GET_CLIENT.format(code))
+
+    def sendMessage(self, code, message):
+        self.__open()
+        self.__session.execute(INSERT_MESSAGE.format(code, message))
+        self.__connection.commit()
+        self.__close()
+        return self.__session.lastrowid
+
+    def getClaimId(self, messageId):
+        return self.__selectOneRow(GET_CLAIM_ID.format(messageId))
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, ext_type, exc_value, traceback):
+        self.cursor.close()
+        if isinstance(exc_value, Exception):
+            self.connection.rollback()
+        else:
+            self.connection.commit()
+        self.connection.close()
+
+
+# if __name__ == '__main__':
+#     with open(DATABASE_FILE) as db:
+#             configFile = json.load(db)
+#     db = Database(configFile["user"], configFile["passwd"], configFile["host"], configFile["port"], configFile["database"])
---
+++ integralcom.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Simple Bot to reply to Telegram messages.
+This program is dedicated to the public domain under the CC0 license.
+This Bot uses the Updater class to handle the bot.
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+import re
+from dbSigesmen import Database
+import json
+import time
+
+MSG_OK = "Mensaje al código {0} envíado correctamente"
+BAD_CODE = "Códgo {0} no pertenece a nuestra base de datos"
+BAD_MSG = "Mensaje incorrecto. Recuerde que es clave o código y luego el mensaje."
+MSG_FROM_TELEGRAM = "Enviado desde Telegram."
+
+REGULAR_EXP = r"\d{4}"
+DATABASE_FILE = "dbConf.json"
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+with open(DATABASE_FILE) as db:
+    configFile = json.load(db)
+db = Database(configFile["user"], configFile["passwd"], configFile["host"], configFile["port"], configFile["database"])
+
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(bot, update):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def help(bot, update):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(bot, update):
+    """Echo the user message."""
+    newMsg = update.message.text.strip()
+    match = re.match(REGULAR_EXP, newMsg)
+    if match:
+        code = match.group()
+        message = re.sub(code, '', newMsg)
+
+        if db.isCodeExists(code):
+            msgId = db.sendMessage(code, "{0}. {1}".format(message, MSG_FROM_TELEGRAM))
+            update.message.reply_text(MSG_OK.format(code))
+        else:
+            update.message.reply_text(BAD_CODE.format(code))
+    else:
+        update.message.reply_text(BAD_MSG)
+
+
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def main():
+    """Start the bot."""
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater("")
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    #dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
